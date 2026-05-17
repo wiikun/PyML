8635d45dcb3dcf0a0d4d8e31123caa63312f07d7
wiikun <qwop90175@gmail.com>
2026-05-17 21:07:29 +0900

    add type hint

--- py_ml.py
+++ py_ml.py
@@ -1,15 +1,16 @@
 import ctypes
 import mmap
+from typing import Callable,Any,Sequence
 
 class BinFun:
     __slots__ = ("__data")
-    def __init__(self,fun,buf):
+    def __init__(self,fun:Callable,buf:mmap.mmap):
         self.__data = (fun,buf)
-    def __call__(self,*args):
+    def __call__(self,*args:Any):
         return self.__data[0](*args)
 
 
-def byte_to_fun(code,ftype):
+def byte_to_fun(code:bytes,ftype:Sequence):
     
     buf = mmap.mmap(-1,len(code),flags=mmap.MAP_ANONYMOUS | mmap.MAP_SHARED
                     ,prot=mmap.PROT_WRITE|mmap.PROT_READ|mmap.PROT_EXEC)
@@ -20,7 +21,7 @@ def byte_to_fun(code,ftype):
     return BinFun(fun,buf)
 
 
-def readff(name,ftype):
+def readff(name:str,ftype:Sequence):
     with open(name,"rb") as f:
         return byte_to_fun(f.read(),ftype)
 
